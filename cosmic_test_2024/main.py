fbd943686db19aaa91f018326eca64189be1c189
Philippa Muston <pm22@sanger.ac.uk>
2024-06-23 08:11:40 +0100

    Got dataframe printing

--- cosmic_test_2024/main.py
+++ cosmic_test_2024/main.py
@@ -6,7 +6,7 @@ import pandas as pd
 def main():
     '''Main function to run the program.''' 
     input_file = Path('../simple_somatic_mutation.open.BLCA-CN.tsv')
-    output_path = Path('../output_folder')
+    output_path = Path('../output_folder/output.tsv')
 
     if input_file.exists():
         pd.set_option('display.max_rows', 500)
@@ -15,8 +15,6 @@ def main():
         dataframe = get_dataframe_from_file(input_file)
         grouped = group_mutations(dataframe)
         print(grouped)
-
-
 
     else:
         raise ValueError("The file does not exist. Please check the path.")
