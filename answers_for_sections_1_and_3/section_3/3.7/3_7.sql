13aa284c89d5807ecdea1e350835bb13fa78b7eb
Philippa Muston <pm22@sanger.ac.uk>
2024-06-24 14:22:37 +0100

    Added section 1 answers

--- answers_for_sections_1_and_3/section_3/3.7/3_7.sql
+++ answers_for_sections_1_and_3/section_3/3.7/3_7.sql
@@ -1,3 +1,9 @@
 --Imagine that we have a table called “some_gene” with only a subset of the gene data. 
 --If I want to join the gene table with this table but display all the genes in the result, 
 --what kind of join would you do?
+
+
+--Left Join would retrieve all the records on the left taable (gene) in this case and the matched records from the right table (some_gene).
+--Example:
+
+SELECT * FROM gene LEFT JOIN some_gene ON gene.ID_GENE = some_gene.ID_GENE;
--- answers_for_sections_1_and_3/section_3/3.8/3_8.sql
+++ answers_for_sections_1_and_3/section_3/3.8/3_8.sql
@@ -1,2 +1,5 @@
 --Imagine that the gene and transcript tables are getting very big and that 
 --joining the two tables get slower and slower. What would you do to improve performances?
+
+--Create an index on the ID_GENE column in the transcript table.
+--Also views specific for each use case which only contain a subset of the columns or rows needed.
